d99628abd71c4a4e73d56731496b87b94fcdfee8
Chris Marie-Angélique <christopher.marieangelique.pro@gmail.com>
2024-06-05 01:02:51 +0200

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, HostBinding, signal } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import AOS from 'aos';
+import { NavigationEnd, Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +12,15 @@ import AOS from 'aos';
 export class AppComponent {
   title = 'Heaven-Flow-Group';
 
-  darkMode = signal<boolean>(false)
-
+  darkMode = signal<boolean>(false);
   currentYear: number = new Date().getFullYear();
+  language: string = 'fr'
+
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('fr');
+    translate.use('fr');
+  }
 
   ngOnInit(): void {
     initFlowbite();
@@ -25,5 +33,16 @@ export class AppComponent {
   @HostBinding('class.dark') get mode() {
     return this.darkMode();
   }
+
+  changeLanguage(language: string): void {
+    if(language === 'fr') {
+      language = 'en'
+    } else {
+      language = 'fr'
+    }
+    this.language = language
+    this.translate.use(language)
+  }
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
